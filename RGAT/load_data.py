ea34da8fac30b73fc286b27b750051432c70030e
Junhong Wan <31362319+wanjunhong0@users.noreply.github.com>
2020-08-12 11:08:51 +0800

    move neg sampling under class Data

--- RGAT/load_data.py
+++ RGAT/load_data.py
@@ -8,17 +8,16 @@ import random
 
 
 class Data(object):
-    def __init__(self, path, seed):
-        random.seed(seed)
+    def __init__(self, path):
         # read file
-        train = pd.read_csv(path + '/train.txt', sep='\t', header=None, names=['head', 'relation', 'tail'],
+        self.train = pd.read_csv(path + '/train.txt', sep='\t', header=None, names=['head', 'relation', 'tail'],
                             keep_default_na=False, encoding='utf-8')
-        valid = pd.read_csv(path + '/valid.txt', sep='\t', header=None, names=['head', 'relation', 'tail'],
+        self.val = pd.read_csv(path + '/valid.txt', sep='\t', header=None, names=['head', 'relation', 'tail'],
                             keep_default_na=False, encoding='utf-8')
-        test = pd.read_csv(path + '/test.txt', sep='\t', header=None, names=['head', 'relation', 'tail'],
+        self.test = pd.read_csv(path + '/test.txt', sep='\t', header=None, names=['head', 'relation', 'tail'],
                             keep_default_na=False, encoding='utf-8')
         # get full entity and relation set
-        data = pd.concat([train, test, valid], axis=0)
+        data = pd.concat([self.train, self.test, self.val], axis=0)
         entity = pd.concat([data['head'], data['tail']], axis=0)
         self.n_entity = entity.nunique()
         self.n_relation = data['relation'].nunique()
@@ -26,51 +25,35 @@ class Data(object):
         entity_map = dict(zip(entity.unique(), range(entity.nunique())))
         relation_map = dict(zip(data['relation'].unique(), range(data['relation'].nunique())))
 
-        train['head'] = train['head'].map(entity_map)
-        train['tail'] = train['tail'].map(entity_map)
-        train['relation'] = train['relation'].map(relation_map)
+        self.train['head'] = self.train['head'].map(entity_map)
+        self.train['tail'] = self.train['tail'].map(entity_map)
+        self.train['relation'] = self.train['relation'].map(relation_map)
 
-        valid['head'] = valid['head'].map(entity_map)
-        valid['tail'] = valid['tail'].map(entity_map)
-        valid['relation'] = valid['relation'].map(relation_map)
+        self.val['head'] = self.val['head'].map(entity_map)
+        self.val['tail'] = self.val['tail'].map(entity_map)
+        self.val['relation'] = self.val['relation'].map(relation_map)
 
-        test['head'] = test['head'].map(entity_map)
-        test['tail'] = test['tail'].map(entity_map)
-        test['relation'] = test['relation'].map(relation_map)
-        # graph
-        self.graph = torch.LongTensor(train.values)
-
-        # train, negative sampling
-        train_neg = neg_sampling(train, self.n_entity, self.n_relation, 'head')
-        train['label'] = 1
-        train_neg['label'] = 0
-        train = pd.concat([train, train_neg], axis=0)
-        self.train = torch.LongTensor(train[['head', 'relation', 'tail']].values)
-        self.label_train = torch.FloatTensor(train['label'].values)
-        # valid, test
-        self.valid = torch.LongTensor(valid.values)
-        self.test = torch.LongTensor(test.values)
+        self.test['head'] = self.test['head'].map(entity_map)
+        self.test['tail'] = self.test['tail'].map(entity_map)
+        self.test['relation'] = self.test['relation'].map(relation_map)
 
 
-def neg_sampling(dataset, n_entity, n_relation, mode):
-    head_relation = dataset.groupby('relation')['head'].apply(set)
-    tail_relation = dataset.groupby('relation')['tail'].apply(set)
-    relation_entity_neg = dict()
-    for i in range(n_relation):
-        relation_entity_neg[i] = list(set(range(n_entity)) - (head_relation[i] | tail_relation[i]))
+    def neg_sampling(self, dataset, predict):
+        head_relation = dataset.groupby('relation')['head'].apply(set)
+        tail_relation = dataset.groupby('relation')['tail'].apply(set)
+        relation_entity_neg = dict()
+        for i in range(self.n_relation):
+            relation_entity_neg[i] = list(set(range(self.n_entity)) - (head_relation[i] | tail_relation[i]))
 
-    head_neg, tail_neg = [], []
-    for i in dataset['relation']:
-        sample = random.choices(relation_entity_neg[i], k=2)
-        head_neg.append(sample[0])
-        tail_neg.append(sample[1])
-    neg = dataset.copy()
-    if mode == 'head':
-        neg['head'] = head_neg
-    if mode == 'tail':
-        neg['tail'] = tail_neg
-    if mode == 'all':
-        neg['head'] = head_neg
-        neg['tail'] = tail_neg
+        head_neg, tail_neg = [], []
+        for i in dataset['relation']:
+            sample = random.choices(relation_entity_neg[i], k=2)
+            head_neg.append(sample[0])
+            tail_neg.append(sample[1])
+        neg = dataset.copy()
+        if predict == 'head':
+            neg['head'] = head_neg
+        if predict == 'tail':
+            neg['tail'] = tail_neg
 
-    return neg
+        return torch.LongTensor(neg.values)
