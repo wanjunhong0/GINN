39d2c81878d60b6f580040c974c8b8ce4e1bd701
Junhong Wan <31362319+wanjunhong0@users.noreply.github.com>
2020-08-31 15:49:11 +0800

    clean

--- RGAT/utils.py
+++ RGAT/utils.py
@@ -6,13 +6,7 @@ import numpy as np
 def label_smoothing(label, label_smoothing):
     return (1.0 - label_smoothing) * label + label_smoothing / label.max()
 
-def prepare_ranking_input(data, n_entity):
-    h = data[:, 0].unsqueeze(1).repeat(1, n_entity).view(-1, 1)
-    r = data[:, 1].unsqueeze(1).repeat(1, n_entity).view(-1, 1)
-    t = torch.LongTensor(range(n_entity)).view(-1, 1).repeat(data.shape[0], 1)
-    return torch.cat([h, r, t], dim=1)
-
-def get_ranking(score, data, predict):
+def get_ranking(score, data, predict='tail'):
     rank = rankdata(score, axis=1)
     if predict == 'tail':
         index = data[:, 2]
