1f850b7994adedfb50fb6e61bd7402f6ee0d7e80
Junhong Wan <31362319+wanjunhong0@users.noreply.github.com>
2020-09-14 15:04:54 +0800

    remove get _ranking

--- RGAT/utils.py
+++ RGAT/utils.py
@@ -1,18 +1,9 @@
 import torch
-from scipy.stats import rankdata 
 import numpy as np
 
 
 def label_smoothing(label, label_smoothing):
     return (1.0 - label_smoothing) * label + (label_smoothing / label.shape[1])
-
-def get_ranking(score, data, predict='tail'):
-    rank = rankdata(score, axis=1)
-    if predict == 'tail':
-        index = data[:, 2]
-    if predict == 'head':
-        index = data[:, 0]
-    return rank[np.arange(len(rank)), index]
-
+    
 def topNhit(rank, n):
     return np.sum(rank <= n) / len(rank)
