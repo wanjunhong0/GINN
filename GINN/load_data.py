51a16dd31a4734ffd76198a2ede24e9e4d4eea48
Junhong Wan <31362319+wanjunhong0@users.noreply.github.com>
2020-10-09 16:03:09 +0800

    fix bug in filter and clean up

--- GINN/load_data.py
+++ GINN/load_data.py
@@ -1,14 +1,11 @@
-import numpy as np
-import scipy.sparse as sp
 import pandas as pd
 import torch
-import torch.nn.functional as F
-import argparse
-import random
 from sklearn.preprocessing import MultiLabelBinarizer
 
 
 class Data(object):
+    """Data loading
+    """
     def __init__(self, path):
         # read file
         train = pd.read_csv(path + '/train.txt', sep='\t', header=None, names=['head', 'relation', 'tail'],
@@ -23,20 +20,12 @@ class Data(object):
         self.n_entity = entity.nunique()
         self.n_relation = data['relation'].nunique()
         # map entities and relations to index
-        entity_map = dict(zip(entity.unique(), range(entity.nunique())))
-        relation_map = dict(zip(data['relation'].unique(), range(data['relation'].nunique())))
+        self.entity_map = dict(zip(entity.unique(), range(entity.nunique())))
+        self.relation_map = dict(zip(data['relation'].unique(), range(data['relation'].nunique())))
 
-        train['head'] = train['head'].map(entity_map)
-        train['tail'] = train['tail'].map(entity_map)
-        train['relation'] = train['relation'].map(relation_map)
-
-        val['head'] = val['head'].map(entity_map)
-        val['tail'] = val['tail'].map(entity_map)
-        val['relation'] = val['relation'].map(relation_map)
-
-        test['head'] = test['head'].map(entity_map)
-        test['tail'] = test['tail'].map(entity_map)
-        test['relation'] = test['relation'].map(relation_map)
+        train = self.map_index(train)
+        val = self.map_index(val)
+        test = self.map_index(test)
 
         self.triple_train = torch.LongTensor(train.values)
         self.triple_val = torch.LongTensor(val.values)
@@ -51,41 +40,53 @@ class Data(object):
         # index for output(groupby [h, r]) to each triple
         self.index_val = [(self.val == i).all(1).nonzero(as_tuple=False).item() for i in self.triple_val[:, :2]]
         self.index_test = [(self.test == i).all(1).nonzero(as_tuple=False).item() for i in self.triple_test[:, :2]]
+        
+    def map_index(self, dataset):
+        """Map entities and relations to index
 
+        Args:
+            dataset (pandas DataFrame): dataset to map
 
+        Returns:
+            (pandas DataFrame): dataset after mapping
+        """
+        dataset['head'] = dataset['head'].map(self.entity_map)
+        dataset['tail'] = dataset['tail'].map(self.entity_map)
+        dataset['relation'] = dataset['relation'].map(self.relation_map)
+        return dataset
 
-    def prepare_input(self, data):
+    def prepare_input(self, dataset):
         """Prepare model input for multiple BCE loss
 
         Args:
-            data (pandas DataFrame): triple to process
+            dataset (pandas DataFrame): triple to process
 
         Returns:
             h_r (torch tensor): unique [h, r] after groupby
             label (torch tensor): onehot t(dim = n_entity) for each [h, r] 
         """
-        data = data.groupby(by=['head', 'relation'], as_index=False).agg(list)
-        h_r = torch.LongTensor(data[['head', 'relation']].values)
+        dataset = dataset.groupby(by=['head', 'relation'], as_index=False).agg(list)
+        h_r = torch.LongTensor(dataset[['head', 'relation']].values)
         mlb = MultiLabelBinarizer(classes=range(self.n_entity))
-        label = torch.FloatTensor(mlb.fit_transform(data['tail'].values))
+        label = torch.FloatTensor(mlb.fit_transform(dataset['tail'].values))
         return h_r, label
         
-    def prepare_filter(self, data, label):
+    def prepare_filter(self, h_r, label):
         """Prepare filter to filter out results in train
 
         Args:
-            data (torch tensor): unique [h, r] after groupby
+            h_r (torch tensor): unique [h, r] after groupby
             label (torch tensor): onehot t(dim = n_entity) for each [h, r]
 
         Returns:
             (torch tensor): matrix only contain 0 and 1, and 0 for the results in train
         """
         filter_ = []
-        for i in range(data.shape[0]):
+        for i in range(h_r.shape[0]):
             try:
-                filter_.append(self.label_train[(self.train == data[i]).all(1).nonzero(as_tuple=False).item()])
-            except :
+                filter_.append(self.label_train[(self.train == h_r[i]).all(1).nonzero(as_tuple=False).item()])
+            except ValueError:
                 filter_.append(label[i])
         filter_ = torch.stack(filter_, dim=0)
-        filter_ = torch.ones_like(filter_) - (filter_ - label)
+        filter_ = torch.ones_like(filter_) - filter_ + torch.mul(label, filter_)
         return filter_
